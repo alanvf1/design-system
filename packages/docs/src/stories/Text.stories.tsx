f8042d3e05feec79baccbbf3b49c43294ca50fdb
alanvf1 <alan.varela404@gmail.com>
2024-07-02 21:22:56 -0300

    feat: core components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,40 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typograph/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis itaque, nesciunt, quod tempore ipsam quia id nemo libero facilis porro officiis delectus, quae voluptatum deserunt minima cum minus debitis cumque.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: 'inline-radio',
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
